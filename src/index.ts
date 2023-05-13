f300557185b388dcaf5698fba4cff1d273c4b0b5
Madosse <madoss@yandex.ru>
2023-05-13 22:59:50 +0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express, {Request, Response} from 'express';
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!999999999999');
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
